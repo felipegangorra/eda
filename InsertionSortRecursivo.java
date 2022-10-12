747c36ab948c2d66c42f6aeb54149df80d732898
gangorra <felipegangorra@gmail.com>
2022-10-12 18:54:20 -0300

    Update

--- InsertionSortRecursivo.java
+++ InsertionSortRecursivo.java
@@ -1,12 +1,28 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-class InserePrimeiro {
+class InsertionSortRecursivo {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String[] sequencia = sc.nextLine().split(" ");
         sc.close();
 
-        
+        System.out.println(Arrays.toString(InsertionRecursivo(sequencia, 1)));
+    }
+
+    public static String[] InsertionRecursivo(String[] sequencia, int i) {
+
+        if(i >= sequencia.length) {return sequencia;}
+
+        int j = i;
+        while (j > 0 && Integer.parseInt(sequencia[j]) < Integer.parseInt(sequencia[j-1])) {
+            String aux = sequencia[j];
+            sequencia[j] = sequencia[j-1];
+            sequencia[j-1] = aux;
+            j -= 1;
+        }
+
+        if(i + 1 < sequencia.length) {System.out.println(Arrays.toString(sequencia));}
+        return InsertionRecursivo(sequencia, i + 1);
     }
 }
--- MarianaEOsLivros.java
+++ MarianaEOsLivros.java
@@ -4,9 +4,22 @@ import java.util.Scanner;
 class InserePrimeiro {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String[] sequencia = sc.nextLine().split(" ");
+        String[] sequencia = sc.nextLine().split(",");
         sc.close();
 
-        
+        for (int i = 1; i < sequencia.length; i++) {
+
+            int j = i;
+            int x = sequencia[j].compareTo(sequencia[j-1]);
+            while (j > 0 && x < 0) {
+                String aux = sequencia[j];
+                sequencia[j] = sequencia[j-1];
+                sequencia[j-1] = aux;
+                j -= 1;
+                System.out.println(Arrays.toString(sequencia));
+            }
+        }
+
+        System.out.println(Arrays.toString(sequencia));
     }
 }
